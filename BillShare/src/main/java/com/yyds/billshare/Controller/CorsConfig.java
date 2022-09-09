71ff1af83751c442c030a3339679f50f578d7ee0
Xie Yuning <xieyn12345@gmail.com>
2022-09-08 21:06:29 -0700

    fix 2 bugs, websock to selves

---
+++ BillShare/src/main/java/com/yyds/billshare/Controller/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.yyds.billshare.Controller;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*") // SpringBoot2.4.0 [allowedOriginPatterns]代替[allowedOrigins]
+                .allowedMethods("*")
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
+}
+
